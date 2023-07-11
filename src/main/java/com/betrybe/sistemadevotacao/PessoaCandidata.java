931ba4e8f04cd85040aba247c21a9cc0a3531298
Brunobmendes <Bruno.mendes2804@gmail.com>
2023-07-10 23:19:58 -0300

    create candidate and voter abstract class

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,44 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+/** class for candidates people object.
+ * @author Bruno Mendes
+ * @version 1.0
+ *
+ */
+public abstract class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
+
+  /** PessoaCandidata constructor.
+   * @param nome String - candidate name
+   * @param numero int - candidate number id
+   */
+
+  public PessoaCandidata(String nome, int numero) {
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVotos() {
+    this.votos += 1;
+  }
 
 }
