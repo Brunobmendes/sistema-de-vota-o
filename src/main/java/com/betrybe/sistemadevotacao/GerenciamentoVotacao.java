f73ee8fcb87b75701964c4db2089394f91f640ae
Brunobmendes <Bruno.mendes2804@gmail.com>
2023-07-11 00:11:43 -0300

    create cadidate and voter methods

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,52 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<PessoaCandidata>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<PessoaEleitora>();
+  private ArrayList<String> cpfsComputados = new ArrayList<String>();
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    PessoaCandidata candidato = new PessoaCandidata(nome, numero) {
+      @Override
+      public int getNumero() {
+        return super.getNumero();
+      }
+    };
+
+    if (pessoasCandidatas.contains(candidato)) {
+      System.out.println("Número da pessoa candidata já utilizado!");
+    } else {
+      pessoasCandidatas.add(candidato);
+    }
+
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    PessoaEleitora eleitor = new PessoaEleitora(nome, cpf) {
+      @Override
+      public String getCpf() {
+        return super.getCpf();
+      }
+    };
+
+    if (pessoasEleitoras.contains(eleitor)) {
+      System.out.println("Pessoa eleitora já cadastrada!");
+    } else {
+      pessoasEleitoras.add(eleitor);
+    }
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
